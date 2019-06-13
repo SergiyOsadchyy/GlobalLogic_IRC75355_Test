3d1be9dc19814664f4b66fb75deb6d8958fd4357
Sergiy Osadchiy <sergiy.osadchyy@gmail.com>
2019-06-13 18:42:08 +0300

    refactor Controller

--- GlobalLogic_IRC75355_Test/Controllers/IController.cs
+++ GlobalLogic_IRC75355_Test/Controllers/IController.cs
@@ -2,6 +2,6 @@ namespace GlobalLogic_IRC75355_Test.Controllers
 {
     public interface IController
     {
-        void Execute(string pathToSource);
+        void Execute(string pathToFolder, string sourceFileName, string resultFileName);
     }
 }
