494dda773205663b3bc8cabe864d520cfc665f90
Sergiy Osadchiy <sergiy.osadchyy@gmail.com>
2019-06-13 19:13:33 +0300

    add BufferedStream

--- GlobalLogic_IRC75355_Test/Services/ReadService.cs
+++ GlobalLogic_IRC75355_Test/Services/ReadService.cs
@@ -5,7 +5,7 @@ namespace GlobalLogic_IRC75355_Test.Services
 {
     public class ReadService : IReadService
     {
-        private string _inputString;
+        private string _inputString = "";
         
         public string Read(string pathToSource)
         {
@@ -16,7 +16,20 @@ namespace GlobalLogic_IRC75355_Test.Services
                     throw new FileNotFoundException();
                 }
                 
-                _inputString = File.ReadAllText(pathToSource);
+                using (FileStream fs = File.Open(pathToSource, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))  
+                {  
+                    using (BufferedStream bs = new BufferedStream(fs))  
+                    {  
+                        using (StreamReader sr = new StreamReader(bs))  
+                        {  
+                            string line;  
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                _inputString = _inputString + line + "'\n'";
+                            }  
+                        }  
+                    }  
+                }
             }
             catch(Exception e)
             {
